cc1c5630804fead782d99a976ab05b52b7d36128
alexbsec <j.a.c.buschinelli@gmail.com>
2025-01-10 17:56:13 -0300

    first pick working

--- src/include/tiles.hpp
+++ src/include/tiles.hpp
@@ -1,7 +1,7 @@
 #ifndef _WFC_TILES_HPP
 #define _WFC_TILES_HPP
 #include <SFML/Graphics.hpp>
-#include <map>
+#include <memory>
 #include <stdexcept>
 #include <string>
 #include <unordered_map>
@@ -32,7 +32,8 @@ enum TileType {
   Wall_Vertical,
   Wall_Horizontal,
   Wall_Void,
-  Floor
+  Floor,
+  None
 };
 
 const umap<TileType, string> TEXTURE_PATHS = {
@@ -41,28 +42,131 @@ const umap<TileType, string> TEXTURE_PATHS = {
     {Wall_BottomRightCorner, "assets/Wall_BottomRightCorner.png"},
     {Wall_BottomLeftCorner, "assets/Wall_BottomLeftCorner.png"},
     {Wall_Vertical, "assets/Wall_Vertical.png"},
-    {Wall_Horizontal, "assets/Wall_Horizontal"},
-    {Floor, "assets/Floor.png"}
+    {Wall_Horizontal, "assets/Wall_Horizontal.png"},
+    {Wall_Void, "assets/Wall_Void.png"},
+    {Floor, "assets/Floor.png"}};
+
+const umap<Position, uset<TileType>> WTRCFriends = {
+    {Top, {Floor, Wall_Void}},
+    {Bottom, {Wall_Vertical}},
+    {Left, {Wall_Horizontal}},
+    {Right, {Floor, Wall_Void}},
+    {TopRight, {Floor, Wall_Void, Wall_BottomLeftCorner}},
+    {TopLeft, {Floor, Wall_Void}},
+    {BottomRight, {Floor, Wall_Void}},
+    {BottomLeft, {Floor}},
+};
+
+const umap<Position, uset<TileType>> WTLCFriends = {
+    {Top, {Floor, Wall_Void}},
+    {Bottom, {Wall_Vertical}},
+    {Left, {Floor, Wall_Void}},
+    {Right, {Wall_Horizontal}},
+    {TopRight, {Floor, Wall_Void}},
+    {TopLeft, {Floor, Wall_Void, Wall_BottomRightCorner}},
+    {BottomRight, {Floor}},
+    {BottomLeft, {Floor, Wall_Void}},
+};
+
+const umap<Position, uset<TileType>> WBRCFriends = {
+    {Top, {Wall_Vertical}},
+    {Bottom, {Floor, Wall_Void}},
+    {Left, {Wall_Horizontal}},
+    {Right, {Floor, Wall_Void}},
+    {TopRight, {Floor, Wall_Void}},
+    {TopLeft, {Floor}},
+    {BottomRight, {Floor, Wall_Void, Wall_TopLeftCorner}},
+    {BottomLeft, {Floor, Wall_Void}},
+};
+
+const umap<Position, uset<TileType>> WBLCFriends = {
+    {Top, {Wall_Vertical}},
+    {Bottom, {Floor, Wall_Void}},
+    {Left, {Floor, Wall_Void}},
+    {Right, {Wall_Horizontal}},
+    {TopRight, {Floor}},
+    {TopLeft, {Floor, Wall_Void}},
+    {BottomRight, {Floor, Wall_Void}},
+    {BottomLeft, {Floor, Wall_Void, Wall_TopRightCorner}},
+};
+
+const umap<Position, uset<TileType>> WVFriends = {
+    {Top, {Wall_Vertical, Wall_TopRightCorner, Wall_TopLeftCorner}},
+    {Bottom, {Wall_Vertical, Wall_BottomRightCorner, Wall_BottomLeftCorner}},
+    {Left, {Floor, Wall_Void}},
+    {Right, {Floor, Wall_Void}},
+    {TopRight, {Floor, Wall_Void, Wall_Horizontal}},
+    {TopLeft, {Floor, Wall_Void, Wall_Horizontal}},
+    {BottomRight, {Floor, Wall_Void, Wall_Horizontal}},
+    {BottomLeft, {Floor, Wall_Void, Wall_Horizontal}},
+};
+
+const umap<Position, uset<TileType>> WHFriends = {
+    {Top, {Floor, Wall_Void}},
+    {Bottom, {Floor, Wall_Void}},
+    {Left, {Wall_Horizontal, Wall_TopLeftCorner, Wall_BottomLeftCorner}},
+    {Right, {Wall_Horizontal, Wall_TopRightCorner, Wall_BottomRightCorner}},
+    {TopRight, {Floor, Wall_Void, Wall_Vertical}},
+    {TopLeft, {Floor, Wall_Void, Wall_Vertical}},
+    {BottomRight, {Floor, Wall_Void, Wall_Vertical}},
+    {BottomLeft, {Floor, Wall_Void, Wall_Vertical}},
+};
+
+const umap<Position, uset<TileType>> VOIDFriends = {
+    {Top,
+     {Wall_Void, Wall_BottomRightCorner, Wall_BottomLeftCorner,
+      Wall_Horizontal}},
+    {Bottom,
+     {Wall_Void, Wall_TopRightCorner, Wall_TopLeftCorner, Wall_Horizontal}},
+    {Left,
+     {Wall_Void, Wall_TopRightCorner, Wall_BottomRightCorner, Wall_Vertical}},
+    {Right,
+     {Wall_Void, Wall_TopLeftCorner, Wall_BottomLeftCorner, Wall_Vertical}},
+    {TopRight,
+     {Wall_Void, Wall_TopLeftCorner, Wall_BottomLeftCorner,
+      Wall_BottomRightCorner, Wall_Vertical, Wall_Horizontal}},
+    {TopLeft,
+     {Wall_Void, Wall_TopRightCorner, Wall_BottomLeftCorner,
+      Wall_BottomRightCorner, Wall_Vertical, Wall_Horizontal}},
+    {BottomRight,
+     {Wall_Void, Wall_TopLeftCorner, Wall_TopRightCorner, Wall_BottomLeftCorner,
+      Wall_Vertical, Wall_Horizontal}},
+    {BottomLeft,
+     {Wall_Void, Wall_TopLeftCorner, Wall_TopRightCorner,
+      Wall_BottomRightCorner, Wall_Vertical, Wall_Horizontal}},
+};
+
+const umap<Position, uset<TileType>> FLOORFriends = {
+    {Top, {Floor, Wall_Horizontal}},
+    {Bottom, {Floor, Wall_Horizontal}},
+    {Left, {Floor, Wall_Vertical}},
+    {Right, {Floor, Wall_Vertical}},
+    {TopRight, {Floor, Wall_TopRightCorner}},
+    {TopLeft, {Floor, Wall_TopLeftCorner}},
+    {BottomRight, {Floor, Wall_BottomRightCorner}},
+    {BottomLeft, {Floor, Wall_BottomLeftCorner}},
 };
 
 struct FriendTiles {
   TileType tileType;
   umap<Position, uset<TileType>> tileTypeFriends;
 
-  FriendTiles(const TileType &type, const umap<Position, uset<TileType>> &friends)
-    : tileType(type), tileTypeFriends(friends) {}
+  FriendTiles(const TileType &type,
+              const umap<Position, uset<TileType>> &friends)
+      : tileType(type), tileTypeFriends(friends) {}
 };
 
 struct Tile {
   int x;
   int y;
-  int index;
   TileType type;
   sf::Sprite sprite;
   sf::Texture texture;
 
-  explicit Tile(const TileType &type, int x, int y)
-      : x(x), y(y), index(-1), type(type) {
+  Tile(const TileType &type, int x, int y) : x(x), y(y), type(type) {
+    if (type == TileType::None)
+      return;
+
     if (TEXTURE_PATHS.find(type) == TEXTURE_PATHS.end()) {
       throw std::runtime_error("TileError: could not load tile because "
                                "TileType is invalid or it does not exits");
@@ -75,10 +179,32 @@ struct Tile {
 
     sprite.setTexture(texture);
   }
+
+  void SetPosition(float x, float y) { sprite.setPosition(x, y); }
 };
 
-uset<TileType> GetFriendTiles(TileType type, Position position);
+struct GridCell {
+  int index;
+  bool colapsed;
+  std::unique_ptr<Tile> tile;
+  uset<TileType> states;
 
-const vector<FriendTiles*>& GetFriendAllTiles(); 
+  GridCell() : index(-1), colapsed(false), tile(nullptr) {
+    states = {Wall_BottomLeftCorner,
+              Wall_BottomRightCorner,
+              Wall_TopLeftCorner,
+              Wall_TopRightCorner,
+              Wall_Vertical,
+              Wall_Horizontal,
+              Wall_Void,
+              Floor};
+  }
+
+  bool operator==(const GridCell &other) const {
+    return states == other.states && colapsed == other.colapsed;
+  }
+
+  void SetTile(std::unique_ptr<Tile> tile) { this->tile = std::move(tile); }
+};
 
 #endif
--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,117 @@
+#include "include/tiles.hpp"
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/VideoMode.hpp>
+#include <algorithm>
 #include <cstdlib>
+#include <cstring>
+#include <iterator>
 #include <iostream>
+#include <numeric>
 
+const int WINDOW_WIDTH = 1920;
+const int WINDOW_HEIGHT = 1080;
+
+const int PIXEL_SIZE = 16;
+
+
+const int UNITS = 32;
+const float SCALE_X = (float)WINDOW_WIDTH / (UNITS * PIXEL_SIZE);
+const float SCALE_Y = (float)WINDOW_HEIGHT / (UNITS * PIXEL_SIZE);
+const float SCALE = std::min(SCALE_X, SCALE_Y);
+
+template <typename Iterator>
+Iterator RandomElement(Iterator begin, Iterator end) {
+  const unsigned long n = std::distance(begin, end);
+  const unsigned long divisor = ((long)RAND_MAX + 1) / n;
+
+  unsigned long k;
+  do {
+    k = rand() / divisor;
+  } while (k >= n);
+
+  std::advance(begin, k);
+  return begin;
+}
+
+void HandleGrid(vector<GridCell> &grid) {
+  vector<size_t> indices(grid.size());
+  std::iota(indices.begin(), indices.end(), 0);
+
+  // Sort so that we know which grid cell has least amount of possible
+  // states to be in
+  std::sort(indices.begin(), indices.end(),
+            [&grid](size_t a, size_t b) {
+            return grid[a].states.size() < grid[b].states.size();
+            });
+
+  // Since its sorted, we can get a minimum number of states
+  // at index 0
+  size_t len = grid[indices[0]].states.size();
+ 
+  // Find the index where the number of states exceeds the minimum
+  // in the case where we have more than one cell with the minimum len
+  auto it = std::find_if(
+    indices.begin(), indices.end(),
+    [&grid, len](size_t idx) { return grid[idx].states.size() > len; });
+
+  // Determine the stopping index (number of cells with minimum states)
+  // this can be 0
+  size_t stopIndex = std::distance(indices.begin(), it);
+
+  size_t randomPick = indices[rand() % (stopIndex + 1)];
+  GridCell &cell = grid[randomPick];
+
+
+  if (cell.states.size() == 1) {
+    cell.colapsed = true;
+  } else {
+    auto pick = RandomElement(cell.states.begin(), cell.states.end());
+    cell.states = uset<TileType>{*pick};
+    cell.colapsed = true;
+  }
+}
+
+void Draw(sf::RenderWindow &window, vector<GridCell> &grid) {
+  for (int j = 0; j < UNITS; j++) {
+    for (int i = 0; i < UNITS; i++) {
+      GridCell &cell = grid[i + j * UNITS];
+      if (cell.colapsed) {
+        // Only one option state
+        TileType type = *cell.states.begin();
+        cell.tile = std::make_unique<Tile>(type, i, j);
+        cell.tile->SetPosition(i * PIXEL_SIZE * SCALE, j * PIXEL_SIZE * SCALE);
+        cell.tile->sprite.setScale(SCALE, SCALE);
+        window.draw(cell.tile->sprite);
+      } 
+    }
+  }
+}
 
 int main() {
-  std::cout << "Hi mom" << std::endl;
+  srand(time(nullptr));
+  vector<GridCell> grid(UNITS * UNITS);
+  sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "WFC");
+  window.setVerticalSyncEnabled(true);
+
+  int counter = 0;
+  for (auto &cell : grid) {
+    cell.index = counter;
+    counter++;
+  }
+  
+  HandleGrid(grid);
+
+  while (window.isOpen()) {
+    sf::Event event;
+    if (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
+    }
+
+    window.clear();
+    Draw(window, grid);
+    window.display();
+  }
   return EXIT_SUCCESS;
 }
