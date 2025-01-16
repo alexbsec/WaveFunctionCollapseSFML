79a9dfca08ffcff88bb5e8df88e8f5567fcd6a53
alexbsec <j.a.c.buschinelli@gmail.com>
2025-01-16 09:25:39 -0300

    definitions of tiles

--- src/tiles.cc
+++ src/tiles.cc
@@ -1,2 +1,19 @@
 #include "include/tiles.hpp"
+#include "include/config.hpp"
 
+Tile::Tile(const int &x, const int &y) 
+  : x(x), y(y) {
+  for (const auto &[key, _] : TILE_RULES) {
+    possibilities.insert(key);
+  }
+
+  entropy = possibilities.size();
+}
+
+void Tile::AddNeighbor(Direction direction, Tile tile) {
+  _neighborTiles[direction] = tile;
+}
+
+Tile Tile::GetNeighbor(Direction direction) const {
+  return _neighborTiles.at(direction);
+}
